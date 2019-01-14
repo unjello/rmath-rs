49aa8f0ec98afb463da9629fa3439a7123cdc387
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-14 06:33:36 -0600

    Add initial SSE implementation of float4

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
+use core::arch::x86_64::{__m128,_mm_set_ps1,_mm_cvtss_f32};
+
+pub struct Vector4 {
+  data: __m128
+}
+
+impl Vector4 {
+  #[inline]
+  pub fn new(v: f32) -> Self {
+    unsafe {
+      let data = _mm_set_ps1(v);  
+      Self {
+        data
+      }
+    }
+  }
+
+  #[inline]
+  pub fn x(&self) -> f32 {
+    unsafe {
+      _mm_cvtss_f32(self.data)
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use assert_approx_eq::assert_approx_eq;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let v = Vector4::new(0.1);
+        assert_approx_eq!(v.x(), 0.1);
     }
 }
