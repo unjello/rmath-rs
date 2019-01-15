0625e215f5af09a816e26684ebc4a8c4a0fefe31
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-01-15 01:52:47 -0600

    Add trait for adding vectors

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use core::arch::x86_64::{__m128,_mm_set_ps1,_mm_cvtss_f32};
+use core::arch::x86_64::{__m128,_mm_set_ps1, _mm_cvtss_f32, _mm_add_ps};
+use core::ops::Add;
 
+#[derive(Debug)]
 pub struct Vector4 {
   data: __m128
 }
@@ -23,14 +25,34 @@ impl Vector4 {
   }
 }
 
+impl Add for Vector4 {
+  type Output = Vector4;
+
+  fn add(self, other: Vector4) -> Vector4 { 
+    unsafe {
+      Self {
+        data : _mm_add_ps(self.data, other.data)
+      }
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use assert_approx_eq::assert_approx_eq;
 
     #[test]
-    fn it_works() {
+    fn can_be_constructed() {
         let v = Vector4::new(0.1);
         assert_approx_eq!(v.x(), 0.1);
+    }
+
+    #[test]
+    fn adding_vectors() {
+        let v1 = Vector4::new(0.1);
+        let v2 = Vector4::new(1.8);
+        let v3 = v1 + v2;
+        assert_approx_eq!(v3.x(), 1.9);
     }
 }
